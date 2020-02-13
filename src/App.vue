393dab8005d6034d67ca012e88c537e667593fea
TaeBbong <mok05289@korea.ac.kr>
2020-02-14 04:02:53 +0900

    Init Project Structure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <HomeHeader></HomeHeader>
+    <HomeInput></HomeInput>
+    <HomeResult></HomeResult>
+    <HomeMore></HomeMore>
+    <HomeFooter></HomeFooter>
   </div>
 </template>
 
 <script>
+import HomeHeader from "./components/HomeHeader";
+import HomeInput from "./components/HomeInput";
+import HomeResult from "./components/HomeResult";
+import HomeMore from "./components/HomeMore";
+import HomeFooter from "./components/HomeFooter";
+
 export default {
-  name: 'App'
-}
+  components: {
+    HomeHeader: HomeHeader,
+    HomeInput: HomeInput,
+    HomeResult: HomeResult,
+    HomeMore: HomeMore,
+    HomeFooter: HomeFooter
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/HomeMore.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>more</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
