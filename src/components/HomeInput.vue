75fcbe6c55eb9c807ff75b484e9acff775f89bed
TaeBbong <mok05289@korea.ac.kr>
2020-02-15 04:00:15 +0900

    Fill Header, Input component

--- src/components/HomeInput.vue
+++ src/components/HomeInput.vue
@@ -1,10 +1,57 @@
 <template>
-  <div>input</div>
+  <div>
+    <div class="inputBox shadow">
+      <input type="text" v-model="newScore" placeholder="현재 점수" />
+    </div>
+    <div class="inputBox shadow">
+      <input type="text" v-model="newFirst" placeholder="1등 횟수" />
+    </div>
+    <div class="inputBox shadow">
+      <input type="text" v-model="newWin" placeholder="4등 이내 횟수" />
+    </div>
+    <div class="inputBox shadow">
+      <input type="text" v-model="newTime" placeholder="플레이 시간" />
+    </div>
+    <div class="submitButton">
+      <button v-on:click="calResult">결과보기</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newScore: "",
+      newFirst: "",
+      newWin: "",
+      newTime: ""
+    };
+  },
+  methods: {
+    calResult() {
+      console.log(this.newScore, this.newFirst, this.newWin, this.newTime);
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.submitButton {
+  height: 50px;
+  border-radius: 5px;
+}
+</style>>
